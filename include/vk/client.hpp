e2fc64cbfb3a052674576068f193ea1403e4de1f
Loveskyrim <dr.konovalov@gmail.com>
2016-11-09 16:26:16 +0300

    Create client.hpp

---
+++ include/vk/client.hpp
@@ -0,0 +1,19 @@
+#ifndef VKCLIENT_CLIENT_HPP
+#define VKCLIENT_CLIENT_HPP
+
+#include <string>
+#include <map>
+
+namespace Vk
+{
+    class Client
+    {
+    public:
+        using dict_t = std::map<std::string, std::string>;
+        Client(dict_t settings) : settings_(settings) {}
+        auto check_connection() -> bool;
+        
+    private:
+        dict_t settings_;
+    };
+}        
