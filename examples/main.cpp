179d4b5aeaa2a135c661d481a565c4e5193f1ec3
Loveskyrim <dr.konovalov@gmail.com>
2016-11-26 22:34:10 +0400

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -15,6 +15,10 @@ int main(int argc, char * argv[])
     std::cout << "https://oauth.vk.com/authorize?client_id=5719956&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=audio&response_type=token&v=5.60" << std::endl;
     std::cout << "Токен: ";
     std::cin >> token;
+	
+	if (!std::cin)
+        std::cerr << "Неверный ввод" << std::endl;
+	
     Vk::Client vk_cl({{"token", token}});
 
     if (vk_cl.check_connection())
@@ -25,6 +29,9 @@ int main(int argc, char * argv[])
     size_t n;
     std::cout << "Введите количество одновременно запускаемых потоков: " << std::endl;
     std::cin >> n;
+	
+	if (!std::cin)
+        std::cerr << "Неверный ввод" << std::endl;
 
     if (n < 1 || n > std::thread::hardware_concurrency())
 	    std::cout << "Неверное число потоков" << std::endl;
